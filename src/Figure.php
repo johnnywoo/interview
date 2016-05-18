73cb82f219c37af65d259579fcc691353306c9ca
Nickolay Kopylov <nickkopylov@gmail.com>
2016-05-18 17:43:44 +0300

    Nickolay Kopylov

--- src/Figure.php
+++ src/Figure.php
@@ -4,6 +4,8 @@ class Figure
 {
     protected $isBlack;
 
+    private $wasMoved = false;
+
     public function __construct($isBlack)
     {
         $this->isBlack = $isBlack;
@@ -13,5 +15,52 @@ class Figure
     public function __toString()
     {
         throw new \Exception("Not implemented");
+    }
+
+    /**
+     * @return bool
+     */
+    public function isBlack()
+    {
+        return $this->isBlack;
+    }
+
+    /**
+     * @return bool
+     */
+    public function wasMoved()
+    {
+        return $this->wasMoved;
+    }
+
+    /**
+     * @param $xFrom
+     * @param $yFrom
+     * @param $xTo
+     * @param $yTo
+     * @return bool
+     * @throws Exception
+     */
+    public function performMove($xFrom, $yFrom, $xTo, $yTo)
+    {
+        if ($this->validateMove($xFrom, $yFrom, $xTo, $yTo)) {
+            $this->wasMoved = true;
+        } else {
+            throw new \Exception("Invalid move");
+        }
+
+        return true;
+    }
+
+    /**
+     * @param $xFrom
+     * @param $yFrom
+     * @param $xTo
+     * @param $yTo
+     * @return mixed
+     */
+    protected function validateMove($xFrom, $yFrom, $xTo, $yTo)
+    {
+        return true;
     }
 }
