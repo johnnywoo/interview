72ab37970113182d0c582320a4a34d0237566767
Aleksandr Galkin <agalkin@agalkin.ru>
2017-01-18 17:32:59 +0300

    Меньше проблем с parse errors

--- chess.php
+++ chess.php
@@ -1,21 +1,21 @@
 <?php
 
-set_exception_handler(function (\Exception $e) {
+try {
+    foreach (glob('src/*.php') as $file) {
+        require_once $file;
+    }
+
+    $desk = new Desk();
+
+    $args = $argv;
+    array_shift($args);
+
+    foreach ($args as $move) {
+        $desk->move($move);
+    }
+
+    $desk->dump();
+} catch (\Exception $e) {
     echo $e->getMessage() . "\n";
     exit(1);
-});
-
-foreach (glob('src/*.php') as $file) {
-    require_once $file;
 }
-
-$desk = new Desk();
-
-$args = $argv;
-array_shift($args);
-
-foreach ($args as $move) {
-    $desk->move($move);
-}
-
-$desk->dump();
